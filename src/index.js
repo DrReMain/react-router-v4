8c9bf49a6f6988c422f539c02e0cf4d1838a9088
luozx <remain@divergers.com>
2017-08-15 17:32:50 +0800

    commit demo3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './views/demo2/App';
+import App from './views/demo3/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
