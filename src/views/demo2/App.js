3403294908d377dfeb050e42a4eb7e4932475b25
luozx <remain@divergers.com>
2017-08-15 15:37:13 +0800

    Initial commit

---
+++ src/views/demo2/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <h2>Accounts</h2>
+                    <ul>
+                        <li><Link to="/netflix">Netflix</Link></li>
+                        <li><Link to="/zillow-group">Zillow Group</Link></li>
+                        <li><Link to="/yahoo">Yahoo</Link></li>
+                        <li><Link to="/modus-create">Modus Create</Link></li>
+                    </ul>
+
+                    <Route path="/:id" component={Child}/>
+
+                </div>
+            </Router>
+        );
+    }
+}
+
+const Child = ({match}) => (
+    <div>
+        <h3>ID: {match.params.id}</h3>
+    </div>
+)
+
+export default App;
